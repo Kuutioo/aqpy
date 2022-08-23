702aba9c82b4e8ec86c0a5aa45083a3cb483f3fb
Kuutioo <ape.hakkarainen@gmail.com>
2022-08-23 14:49:19 +0300

    Pyshark sometimes just chops of the packet if the length is over like 1500. Need away to get around that

--- src/temp.py
+++ src/temp.py
@@ -6,26 +6,28 @@ from jsonstream import loads
 def capture_packets(event_loop):
     asyncio.set_event_loop(event_loop)
     capture = pyshark.LiveCapture(interface='Ethernet', bpf_filter='tcp port 5588', eventloop=event_loop)
-    for packet in capture.sniff_continuously(50):
+    for packet in capture.sniff_continuously():
         if 'DATA' in str(packet.layers):
             payload = packet.tcp.payload
             payload = payload.replace(':', '').replace('00', '')
-            decoded_payload = bytes.fromhex(payload).decode('ASCII')
+            decoded_payload = bytes.fromhex(payload).decode('UTF-8')
             
             if '%' in decoded_payload:
                 continue
-            it = loads(decoded_payload)
+            
+            # decoded_payload = decoded_payload.replace("'s", 's')
+            
+            print(decoded_payload)
+            '''it = loads(decoded_payload)
             for element in list(it):
-                # element = element
                 print(element)
-                '''b = element['b']
+                b = element['b']
                 o = b['o']
                 cmd = o['cmd']
                 if cmd == None:
                     continue
                 if cmd == 'addItems':
                     print('Command is add items')'''
-                #print(cmd)
             
     capture.close()
        
